92327e590d5a30d8773439542f4d0ba1ec9de0bd
BJ Anderson <Brayden.Anderson2003@gmail.com>
2023-08-29 23:07:37 -0500

    Added Colors, Rotation, Cleared Rows, Game Over

--- TetrisGame.java
+++ TetrisGame.java
@@ -8,6 +8,8 @@ import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TetrisGame extends JPanel implements ActionListener, KeyListener {
     private final int BOARD_WIDTH = 10;
@@ -16,6 +18,8 @@ public class TetrisGame extends JPanel implements ActionListener, KeyListener {
     private int[][] board = new int[BOARD_HEIGHT][BOARD_WIDTH];
     private Timer timer;
     private Tetromino currentPiece;
+
+    private Map<Integer, Color> shapeColorMap = new HashMap<>();
 
     public TetrisGame() {
         timer = new Timer(500, this);
@@ -26,7 +30,37 @@ public class TetrisGame extends JPanel implements ActionListener, KeyListener {
         addKeyListener(this);
         setFocusable(true);
 
+        shapeColorMap.put(0, Color.BLACK);
+        shapeColorMap.put(1, Color.CYAN);
+        shapeColorMap.put(2, Color.MAGENTA);
+        shapeColorMap.put(3, Color.GREEN);
+        shapeColorMap.put(4, Color.RED);
+        shapeColorMap.put(5, Color.YELLOW);
+        shapeColorMap.put(6, Color.ORANGE);
+
         timer.start();
+    }
+
+    private void clearFilledRows() {
+        for (int row = BOARD_HEIGHT - 1; row >= 0; row--) {
+            boolean rowIsFilled = true;
+            for (int col = 0; col < BOARD_WIDTH; col++) {
+                if (board[row][col] == 0) {
+                    rowIsFilled = false;
+                    break;
+                }
+            }
+
+            if (rowIsFilled) {
+                for (int r = row; r > 0; r--) {
+                    System.arraycopy(board[r - 1], 0, board[r], 0, BOARD_WIDTH);
+                }
+                for (int col = 0; col < BOARD_WIDTH; col++) {
+                    board[0][col] = 0;
+                }
+                row++; // Check the same row again since we shifted rows down
+            }
+        }
     }
 
     @Override
@@ -44,7 +78,7 @@ public class TetrisGame extends JPanel implements ActionListener, KeyListener {
         } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
             movePieceDown();
         } else if (e.getKeyCode() == KeyEvent.VK_UP) {
-            rotatePiece();
+            currentPiece.rotate();
         }
 
         repaint();
@@ -62,14 +96,14 @@ public class TetrisGame extends JPanel implements ActionListener, KeyListener {
         if (collision()) {
             currentPiece.move(0, -1);
             currentPiece.merge();
-            currentPiece = new Tetromino();
-        }
-    }
+            clearFilledRows();
 
-    private void rotatePiece() {
-        currentPiece.rotate();
-        if (collision()) {
-            currentPiece.rotateBack();
+            if (currentPiece.y <= 0) {
+                gameOver();
+                return;
+            }
+
+            currentPiece = new Tetromino();
         }
     }
 
@@ -88,13 +122,21 @@ public class TetrisGame extends JPanel implements ActionListener, KeyListener {
         return false;
     }
 
+    private void gameOver() {
+        // Implement game over logic, such as displaying a message or resetting the game
+        // For instance:
+        timer.stop();
+        System.out.println("Game Over");
+    }
+
     private class Tetromino {
         private int[][] shape;
         private int x, y;
-        private int rotation;
+        private int shapeIdentifier;
 
         private final int[][][] SHAPES = {
                 // Tetromino shapes and rotations
+                {}, // Empty Shape
                 { { 1, 1, 1, 1 } }, // I shape
                 { { 1, 1, 1 }, { 0, 1, 0 } }, // T shape
                 { { 1, 1, 0 }, { 0, 1, 1 } }, // S shape
@@ -105,10 +147,11 @@ public class TetrisGame extends JPanel implements ActionListener, KeyListener {
         };
 
         public Tetromino() {
-            shape = SHAPES[(int) (Math.random() * SHAPES.length)];
+
+            shapeIdentifier = (int) ((Math.random() * SHAPES.length) + 1);
+            shape = SHAPES[shapeIdentifier];
             x = BOARD_WIDTH / 2 - shape[0].length / 2;
             y = 0;
-            rotation = 0;
         }
 
         public void move(int dx, int dy) {
@@ -117,17 +160,32 @@ public class TetrisGame extends JPanel implements ActionListener, KeyListener {
         }
 
         public void rotate() {
-            rotation = (rotation + 1) % 4;
-            int[][] newShape = SHAPES[rotation];
-            if (x + newShape[0].length > BOARD_WIDTH) {
-                x = BOARD_WIDTH - newShape[0].length;
+            int[][] newShape = new int[shape[0].length][shape.length];
+            for (int row = 0; row < shape.length; row++) {
+                for (int col = 0; col < shape[row].length; col++) {
+                    newShape[col][shape.length - 1 - row] = shape[row][col];
+                }
             }
-            shape = newShape;
+
+            if (!collisionAfterRotation(newShape)) {
+                shape = newShape;
+            }
         }
 
-        public void rotateBack() {
-            rotation = (rotation - 1 + 4) % 4;
-            shape = SHAPES[rotation];
+        private boolean collisionAfterRotation(int[][] newShape) {
+            for (int row = 0; row < newShape.length; row++) {
+                for (int col = 0; col < newShape[row].length; col++) {
+                    if (newShape[row][col] != 0) {
+                        int boardX = x + col;
+                        int boardY = y + row;
+                        if (boardX < 0 || boardX >= BOARD_WIDTH || boardY >= BOARD_HEIGHT
+                                || board[boardY][boardX] != 0) {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
         }
 
         public void merge() {
@@ -153,6 +211,15 @@ public class TetrisGame extends JPanel implements ActionListener, KeyListener {
         // Unused
     }
 
+    private void drawBlock(Graphics g, int row, int col, Color color) {
+        int x = col * BLOCK_SIZE;
+        int y = row * BLOCK_SIZE;
+        g.setColor(color);
+        g.fillRect(x, y, BLOCK_SIZE, BLOCK_SIZE);
+        g.setColor(Color.BLACK);
+        g.drawRect(x, y, BLOCK_SIZE, BLOCK_SIZE);
+    }
+
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -161,12 +228,14 @@ public class TetrisGame extends JPanel implements ActionListener, KeyListener {
         for (int row = 0; row < BOARD_HEIGHT; row++) {
             for (int col = 0; col < BOARD_WIDTH; col++) {
                 int blockValue = board[row][col];
-                if (blockValue != 0) {
+                if (row == 0) {
+                    g.setColor(Color.RED); // Highlight top row in red
+                } else {
                     g.setColor(getBlockColor(blockValue));
-                    g.fillRect(col * BLOCK_SIZE, row * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
-                    g.setColor(Color.BLACK);
-                    g.drawRect(col * BLOCK_SIZE, row * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
                 }
+                g.fillRect(col * BLOCK_SIZE, row * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+                g.setColor(Color.BLACK);
+                g.drawRect(col * BLOCK_SIZE, row * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
             }
         }
 
@@ -174,36 +243,16 @@ public class TetrisGame extends JPanel implements ActionListener, KeyListener {
         for (int row = 0; row < currentPiece.shape.length; row++) {
             for (int col = 0; col < currentPiece.shape[row].length; col++) {
                 if (currentPiece.shape[row][col] != 0) {
-                    int x = (currentPiece.x + col) * BLOCK_SIZE;
-                    int y = (currentPiece.y + row) * BLOCK_SIZE;
-                    g.setColor(getBlockColor(currentPiece.shape[row][col]));
-                    g.fillRect(x, y, BLOCK_SIZE, BLOCK_SIZE);
-                    g.setColor(Color.BLACK);
-                    g.drawRect(x, y, BLOCK_SIZE, BLOCK_SIZE);
+                    int x = currentPiece.x + col;
+                    int y = currentPiece.y + row;
+                    drawBlock(g, y, x, getBlockColor(currentPiece.shapeIdentifier));
                 }
             }
         }
     }
 
-    private Color getBlockColor(int value) {
-        // Define a mapping of block values to colors
-        switch (value) {
-            case 1:
-                return Color.CYAN; // I shape
-            case 2:
-                return Color.MAGENTA; // T shape
-            case 3:
-                return Color.GREEN; // S shape
-            case 4:
-                return Color.RED; // Z shape
-            case 5:
-                return Color.YELLOW; // O shape
-            case 6:
-                return Color.ORANGE; // L shape
-            // Add more cases for additional shapes
-            default:
-                return Color.BLACK; // Empty space or unexpected value
-        }
+    private Color getBlockColor(int shapeIdentifier) {
+        return shapeColorMap.getOrDefault(shapeIdentifier, Color.BLACK);
     }
 
     public static void main(String[] args) {
